e9db016da58f099d749280359660a67da0d51104
JosueMinchez-M <josueminchezm@gmail.com>
2020-09-26 22:27:39 -0600

    Se crea la vista que el usuario verá y usara para ingresar los valores

---
+++ src/main/java/com/mycompany/calculadoraweb/DatosEntrada.java
@@ -0,0 +1,26 @@
+package com.mycompany.calculadoraweb;
+
+/**
+ *
+ * @author joshua
+ */
+public class DatosEntrada {
+    
+    private int numero1;
+    private int numero2;
+
+    public DatosEntrada(int numero1, int numero2) {
+        this.numero1 = numero1;
+        this.numero2 = numero2;
+    }
+
+    public int getNumero1() {
+        return numero1;
+    }
+
+    public int getNumero2() {
+        return numero2;
+    }
+    
+    
+}
---
+++ src/main/java/com/mycompany/calculadoraweb/Operacion.java
@@ -0,0 +1,14 @@
+package com.mycompany.calculadoraweb;
+
+/**
+ *
+ * @author joshua
+ */
+public class Operacion {
+    
+    DatosEntrada datosEntrada;
+ 
+    public int suma(int numero1, int numero2){
+        return numero1 + numero2;   
+    }
+}
